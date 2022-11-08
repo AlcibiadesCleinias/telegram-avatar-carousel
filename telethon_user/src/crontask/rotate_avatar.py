8e4078e25e719eab02e0302a4f40fc757d4b412c
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2022-11-08 19:58:31 +0300

    fixes for next image

--- telethon_user/src/crontask/rotate_avatar.py
+++ telethon_user/src/crontask/rotate_avatar.py
@@ -1,17 +1,21 @@
+import logging
+
 from telethon import TelegramClient
 
 from config.misc import redis
-from config.settings import settings
 from crontask.base import CronTaskBase
 from telegram.change_avatar import change_avatar
+
+logger = logging.getLogger(__name__)
 
 
 def redis_key_to_current_image(phone: str):
     return f'current_image:{phone}'
 
 
-def _get_next_image(phone: str, images: list[str]) -> str:
-    current_image = redis.get(redis_key_to_current_image(phone))
+async def _get_next_image(phone: str, images: list[str]) -> str:
+    current_image = await redis.get(redis_key_to_current_image(phone))
+    logger.info('Current image according to Redis is %s', current_image)
     if not current_image:
         return images[0]
 
@@ -21,9 +25,11 @@ def _get_next_image(phone: str, images: list[str]) -> str:
 
 
 async def _change_image_to_next(client: TelegramClient, phone: str, image_paths: list[str]):
-    new_image = _get_next_image(phone, image_paths)
+    new_image = await _get_next_image(phone, image_paths)
+    logger.info('Next image %s to change', new_image)
     await change_avatar(client, new_image)
-    return redis.set(redis_key_to_current_image(phone), new_image)
+    image_set = await redis.set(redis_key_to_current_image(phone), new_image)
+    return image_set
 
 
 class RotateAvatarTask(CronTaskBase):
