dcd7cabdb1a2ee8d2c7a9e1614c288a131cf4e1a
alcibiadescleinias <ivan.cheprasov@phystech.edu>
2022-11-10 19:15:11 +0300

    delete old app avatar in the end of rotation

--- telethon_user/src/telegram/change_avatar.py
+++ telethon_user/src/telegram/change_avatar.py
@@ -17,28 +17,35 @@ async def change_avatar(client_inited: 'TelegramClient', image_path: str, delete
     - delete previous if requested
     """
     async with await client_inited() as bot:
-        if delete_previous:
-            logger.info('Delete previous photo...')
-            await bot(DeletePhotosRequest(await bot.get_profile_photos("me", limit=1)))
-
-        logger.info('Upload new photo...')
-
         telegram_file_cache = TelegramFileCacheStorage(redisdb=redis, file_name=image_path)
         telegram_file_cached = await telegram_file_cache.get()
+        changed_avatar = None
+
+        logger.info('Start uploading new photo...')
         if telegram_file_cached:
             logger.info('Found file %s in redis cache, use it...', image_path)
             tg_file = await telegram_file_cache.get()
             upload_file_request = UploadProfilePhotoRequest(tg_file)
             try:
-                return await bot(upload_file_request)
+                changed_avatar = await bot(upload_file_request)
             except FilePart0MissingError:
                 logger.warning('Could not use cached file due to FilePart0MissingError, continue...')
                 pass
 
-        logger.info('Upload the file %s...', image_path)
-        upload_file_request = UploadProfilePhotoRequest(
-            await bot.upload_file(image_path)
-        )
-        logger.info('Save tg file %s into Redis cache...', image_path)
-        await telegram_file_cache.save(upload_file_request.file)
-        return await bot(upload_file_request)
+        if not changed_avatar:
+            logger.info('Upload new file %s to telegram...', image_path)
+            upload_file_request = UploadProfilePhotoRequest(
+                await bot.upload_file(image_path)
+            )
+            logger.info('Save tg file %s into Redis cache...', image_path)
+            await telegram_file_cache.save(upload_file_request.file)
+            changed_avatar = await bot(upload_file_request)
+
+        # delete previous old photo in the end of rotation action.
+        if delete_previous:
+            logger.info('Delete previous photo after uploaded the new one...')
+            photos = await bot.get_profile_photos("me", offset=1, limit=1)
+            logger.info('Fetched previous photos, got %s', photos)
+            await bot(DeletePhotosRequest(photos))
+
+        return changed_avatar
